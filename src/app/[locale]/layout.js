3282d6a929b8e178e84f5e3c11b211eaf0a8368c
YossefMohammedM <y2587799@gmail.com>
2025-06-29 21:05:01 +0300

    yousef

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -41,7 +41,7 @@ export default async function RootLayout({ children, params }) {
             <FaviconSwitcher />
             <Navbar />
             {children}
-            <ProductsSection />
+            {/* <ProductsSection /> */}
             <Footer />
           </ThemeProvider>
         </NextIntlClientProvider>
